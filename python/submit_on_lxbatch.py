6f61504ceef4c796e05afb60e42b47c1a85a8b3b
Luca Brianza <brianza@hercules.mib.infn.it>
2015-01-24 18:03:03 +0100

    make ntuples identical to the old ones (run1)

--- python/submit_on_lxbatch.py
+++ python/submit_on_lxbatch.py
@@ -8,21 +8,23 @@ import time
 import subprocess
 
 currentDir = os.getcwd();
-CMSSWDir = currentDir+"/..";
-ReducedTreeDir = "../";
+CMSSWDir = currentDir+"../";
+ReducedTreeDir = "";
 
-name = ["WJets","TTBar"];
+name = ["WJets","TTbar","RSGraviton1000"];
+category = ["mu","el"];
 
-for i in range(len(name)):
-    fn = "Job/Job_"+name[i];
-    outScript = open(fn+".sh","w");
-    command = "python python/produceWWNtuples.py -i "+ReducedTreeDir+"ReducedSelection_"+name[i]+".root -o WWTree_"+name[i]+".root";
-    outScript.write('#!/bin/bash');
-    outScript.write("\n"+'cd '+CMSSWDir);
-    outScript.write("\n"+'eval `scram runtime -sh`');
-    outScript.write("\n"+'cd '+currentDir);
-    outScript.write("\n"+"unbuffer "+command+" > "+fn+"_output.txt");
-    outScript.close();
-    os.system("chmod 777 "+currentDir+"/"+fn+".sh");
-#    os.system("qsub -V -d "+currentDir+" -q longcms "+currentDir+"/"+fn+".sh");
-    os.system("bsub -q 8nh -cwd "+currentDir+" "+fn+".sh");
+for a in range(len(category)):
+    for i in range(len(name)):
+        fn = "Job/Job_"+name[i]+"_"+category[a];
+        outScript = open(fn+".sh","w");
+        command = "python python/produceWWNtuples.py -n ReducedSelection_"+name[i]+".root -o WWTree_"+name[i]+"_"+category[a]+".root -l "+category[a];
+        outScript.write('#!/bin/bash');
+        outScript.write("\n"+'cd '+CMSSWDir);
+        outScript.write("\n"+'eval `scram runtime -sh`');
+        outScript.write("\n"+'cd '+currentDir);
+        outScript.write("\n"+"unbuffer "+command+" > "+fn+"_output.txt");
+        outScript.close();
+        os.system("chmod 777 "+currentDir+"/"+fn+".sh");
+    	os.system("bsub -q 8nh -cwd "+currentDir+" "+fn+".sh");
+
