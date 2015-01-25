e5bdfcfd200b9ff966a52a9cc4c1e3b2df3f2f51
Luca Brianza <brianza@hercules.mib.infn.it>
2015-01-25 11:55:56 +0100

    add VBF infos

--- interface/setOutputTree.h
+++ interface/setOutputTree.h
@@ -49,54 +49,20 @@ extern float v_phi;
 extern float v_mt;
 extern float mass_lvj_type0;
 extern float mass_lvj_type2;
-
-// List of branches
-extern TBranch *b_event_runNo;
-extern TBranch *b_event;
-extern TBranch *b_njets;
-extern TBranch *b_nPV;
-extern TBranch *b_pfMET;
-extern TBranch *b_pfMET_Phi;
-extern TBranch *b_nu_pz_type0;
-extern TBranch *b_nu_pz_type2;
-extern TBranch *b_l_pt;
-extern TBranch *b_l_eta;
-extern TBranch *b_l_phi;
-extern TBranch *b_l_e;
-extern TBranch *b_ungroomed_jet_pt;
-extern TBranch *b_ungroomed_jet_eta;
-extern TBranch *b_ungroomed_jet_phi;
-extern TBranch *b_ungroomed_jet_e;
-extern TBranch *b_jet_mass_pr;
-extern TBranch *b_jet_mass_tr;
-extern TBranch *b_jet_mass_fi;
-extern TBranch *b_jet_tau2tau1;
-/*extern TBranch *b_genBosonPdgId[10];
-extern TBranch *b_genBosonPt[10];
-extern TBranch *b_genBosonEta[10];
-extern TBranch *b_genBosonPhi[10];
-extern TBranch *b_genBosonE[10];
-extern TBranch *b_genLeptonPdgId[10];
-extern TBranch *b_genLeptonPt[10];
-extern TBranch *b_genLeptonEta[10];
-extern TBranch *b_genLeptonPhi[10];
-extern TBranch *b_genLeptonE[10];
-extern TBranch *b_genNuPt[10];
-extern TBranch *b_genNuEta[10];
-extern TBranch *b_genNuPhi[10];
-extern TBranch *b_genNuE[10];
-*/
-extern TBranch *b_deltaR_lak8jet;
-extern TBranch *b_deltaphi_METak8jet;
-extern TBranch *b_deltaphi_Vak8jet;
-extern TBranch *b_v_pt;
-extern TBranch *b_v_eta;
-extern TBranch *b_v_phi;
-extern TBranch *b_v_mt;
-extern TBranch *b_mass_lvj_type0;
-extern TBranch *b_mass_lvj_type2;
-
-void InitRecoTree(TTree* nt);
+extern float vbf_maxpt_j1_pt;
+extern float vbf_maxpt_j1_eta;
+extern float vbf_maxpt_j1_phi;
+extern float vbf_maxpt_j1_e;
+extern float vbf_maxpt_j1_bDiscriminatorCSV;
+extern float vbf_maxpt_j2_pt;
+extern float vbf_maxpt_j2_eta;
+extern float vbf_maxpt_j2_phi;
+extern float vbf_maxpt_j2_e;
+extern float vbf_maxpt_j2_bDiscriminatorCSV;
+extern float vbf_maxpt_jj_pt;
+extern float vbf_maxpt_jj_eta;
+extern float vbf_maxpt_jj_phi;
+extern float vbf_maxpt_jj_m;
 
 void init();
 
