7a28facebac5f4ec36c48788d02e2a9813fe6523
Luca Brianza <brianza@hercules.mib.infn.it>
2015-02-03 11:13:42 +0100

    update

--- src/setOutputTree.cc
+++ src/setOutputTree.cc
@@ -1,56 +1,15 @@
 #include "../interface/setOutputTree.h"
 
-// Declaration of leaf types
-int event_runNo;
-int event;
-int njets;
-int nPV;
-float pfMET;
-float pfMET_Phi;
-float nu_pz_type0;
-float nu_pz_type2;
-float l_pt;
-float l_eta;
-float l_phi;
-float l_e;
-float ungroomed_jet_pt;
-float ungroomed_jet_eta;
-float ungroomed_jet_phi;
-float ungroomed_jet_e;
-float jet_mass_pr;
-float jet_mass_tr;
-float jet_mass_fi;
-float jet_tau2tau1;
-float W_pt_gen;
-float W_pz_gen;
-float gen_GravMass;
-float nu_pz_gen;
-float deltaR_lak8jet;
-float deltaphi_METak8jet;
-float deltaphi_Vak8jet;
-float v_pt;
-float v_eta;
-float v_phi;
-float v_mt;
-float mass_lvj_type0;
-float mass_lvj_type2;
-float vbf_maxpt_j1_pt;
-float vbf_maxpt_j1_eta;
-float vbf_maxpt_j1_phi;
-float vbf_maxpt_j1_e;
-float vbf_maxpt_j1_bDiscriminatorCSV;
-float vbf_maxpt_j2_pt;
-float vbf_maxpt_j2_eta;
-float vbf_maxpt_j2_phi;
-float vbf_maxpt_j2_e;
-float vbf_maxpt_j2_bDiscriminatorCSV;
-float vbf_maxpt_jj_pt;
-float vbf_maxpt_jj_eta;
-float vbf_maxpt_jj_phi;
-float vbf_maxpt_jj_m;
+setOutputTree::setOutputTree(TTree* outTree){
+  fTree = outTree;
+  setBranches();
+}
 
+setOutputTree::~setOutputTree(){
+  delete fTree;
+}
 
-void init()
+void setOutputTree::initializeVariables()
 {
   event_runNo=-999;
   event=-999;
@@ -101,54 +60,54 @@ void init()
   vbf_maxpt_jj_m=-999;
 }
 
-void SetOutTree(TTree* outTree)
+void setOutputTree::setBranches()
 {
-  outTree->Branch("event_runNo",&event_runNo,"event_runNo/I");
-  outTree->Branch("event",&event,"event/I");
-  outTree->Branch("njets",&njets,"njets/I");
-  outTree->Branch("nPV",&nPV,"nPV/I");
-  outTree->Branch("pfMET",&pfMET,"pfMET/F");
-  outTree->Branch("pfMET_Phi",&pfMET_Phi,"pfMET_Phi/F");
-  outTree->Branch("nu_pz_type0",&nu_pz_type0,"nu_pz_type0/F");
-  outTree->Branch("nu_pz_type2",&nu_pz_type2,"nu_pz_type2/F");
-  outTree->Branch("l_pt",&l_pt,"l_pt/F");
-  outTree->Branch("l_eta",&l_eta,"l_eta/F");
-  outTree->Branch("l_phi",&l_phi,"l_phi/F");
-  outTree->Branch("l_e",&l_e,"l_e/F");
-  outTree->Branch("ungroomed_jet_pt",&ungroomed_jet_pt,"ungroomed_jet_pt/F");
-  outTree->Branch("ungroomed_jet_eta",&ungroomed_jet_eta,"ungroomed_jet_eta/F");
-  outTree->Branch("ungroomed_jet_phi",&ungroomed_jet_phi,"ungroomed_jet_phi/F");
-  outTree->Branch("ungroomed_jet_e",&ungroomed_jet_e,"ungroomed_jet_e/F");
-  outTree->Branch("jet_mass_pr",&jet_mass_pr,"jet_mass_pr");
-  outTree->Branch("jet_mass_tr",&jet_mass_tr,"jet_mass_tr");
-  outTree->Branch("jet_mass_fi",&jet_mass_fi,"jet_mass_fi");
-  outTree->Branch("jet_tau2tau1",&jet_tau2tau1,"jet_tau2tau1");
-  outTree->Branch("W_pt_gen",&W_pt_gen,"W_pt_gen");
-  outTree->Branch("W_pz_gen",&W_pz_gen,"W_pz_gen");
-  outTree->Branch("gen_GravMass",&gen_GravMass,"gen_GravMass");
-  outTree->Branch("nu_pz_gen",&nu_pz_gen,"nu_pz_gen");
-  outTree->Branch("deltaR_lak8jet",&deltaR_lak8jet,"deltaR_lak8jet/F");
-  outTree->Branch("deltaphi_METak8jet",&deltaphi_METak8jet,"deltaphi_METak8jet/F");
-  outTree->Branch("deltaphi_Vak8jet",&deltaphi_Vak8jet,"deltaphi_Vak8jet/F");
-  outTree->Branch("v_pt",&v_pt,"v_pt/F");
-  outTree->Branch("v_eta",&v_eta,"v_eta/F");
-  outTree->Branch("v_phi",&v_phi,"v_phi/F");
-  outTree->Branch("v_mt",&v_mt,"v_mt/F");
-  outTree->Branch("mass_lvj_type0",&mass_lvj_type0,"mass_lvj_type0/F");
-  outTree->Branch("mass_lvj_type2",&mass_lvj_type2,"mass_lvj_type2/F");
-  outTree->Branch("vbf_maxpt_j1_pt",&vbf_maxpt_j1_pt,"vbf_maxpt_j1_pt/F");
-  outTree->Branch("vbf_maxpt_j1_eta",&vbf_maxpt_j1_eta,"vbf_maxpt_j1_eta/F");
-  outTree->Branch("vbf_maxpt_j1_phi",&vbf_maxpt_j1_phi,"vbf_maxpt_j1_phi/F");
-  outTree->Branch("vbf_maxpt_j1_e",&vbf_maxpt_j1_e,"vbf_maxpt_j1_e/F");
-  outTree->Branch("vbf_maxpt_j1_bDiscriminatorCSV",&vbf_maxpt_j1_bDiscriminatorCSV,"vbf_maxpt_j1_bDiscriminatorCSV/F");
-  outTree->Branch("vbf_maxpt_j2_pt",&vbf_maxpt_j2_pt,"vbf_maxpt_j2_pt/F");
-  outTree->Branch("vbf_maxpt_j2_eta",&vbf_maxpt_j2_eta,"vbf_maxpt_j2_eta/F");
-  outTree->Branch("vbf_maxpt_j2_phi",&vbf_maxpt_j2_phi,"vbf_maxpt_j2_phi/F");
-  outTree->Branch("vbf_maxpt_j2_e",&vbf_maxpt_j2_e,"vbf_maxpt_j2_e/F");
-  outTree->Branch("vbf_maxpt_j2_bDiscriminatorCSV",&vbf_maxpt_j2_bDiscriminatorCSV,"vbf_maxpt_j2_bDiscriminatorCSV/F");
-  outTree->Branch("vbf_maxpt_jj_pt",&vbf_maxpt_jj_pt,"vbf_maxpt_jj_pt/F");
-  outTree->Branch("vbf_maxpt_jj_eta",&vbf_maxpt_jj_eta,"vbf_maxpt_jj_eta/F");
-  outTree->Branch("vbf_maxpt_jj_phi",&vbf_maxpt_jj_phi,"vbf_maxpt_jj_phi/F");
-  outTree->Branch("vbf_maxpt_jj_m",&vbf_maxpt_jj_m,"vbf_maxpt_jj_m/F");
+  fTree->Branch("event_runNo",&event_runNo,"event_runNo/I");
+  fTree->Branch("event",&event,"event/I");
+  fTree->Branch("njets",&njets,"njets/I");
+  fTree->Branch("nPV",&nPV,"nPV/I");
+  fTree->Branch("pfMET",&pfMET,"pfMET/F");
+  fTree->Branch("pfMET_Phi",&pfMET_Phi,"pfMET_Phi/F");
+  fTree->Branch("nu_pz_type0",&nu_pz_type0,"nu_pz_type0/F");
+  fTree->Branch("nu_pz_type2",&nu_pz_type2,"nu_pz_type2/F");
+  fTree->Branch("l_pt",&l_pt,"l_pt/F");
+  fTree->Branch("l_eta",&l_eta,"l_eta/F");
+  fTree->Branch("l_phi",&l_phi,"l_phi/F");
+  fTree->Branch("l_e",&l_e,"l_e/F");
+  fTree->Branch("ungroomed_jet_pt",&ungroomed_jet_pt,"ungroomed_jet_pt/F");
+  fTree->Branch("ungroomed_jet_eta",&ungroomed_jet_eta,"ungroomed_jet_eta/F");
+  fTree->Branch("ungroomed_jet_phi",&ungroomed_jet_phi,"ungroomed_jet_phi/F");
+  fTree->Branch("ungroomed_jet_e",&ungroomed_jet_e,"ungroomed_jet_e/F");
+  fTree->Branch("jet_mass_pr",&jet_mass_pr,"jet_mass_pr");
+  fTree->Branch("jet_mass_tr",&jet_mass_tr,"jet_mass_tr");
+  fTree->Branch("jet_mass_fi",&jet_mass_fi,"jet_mass_fi");
+  fTree->Branch("jet_tau2tau1",&jet_tau2tau1,"jet_tau2tau1");
+  fTree->Branch("W_pt_gen",&W_pt_gen,"W_pt_gen");
+  fTree->Branch("W_pz_gen",&W_pz_gen,"W_pz_gen");
+  fTree->Branch("gen_GravMass",&gen_GravMass,"gen_GravMass");
+  fTree->Branch("nu_pz_gen",&nu_pz_gen,"nu_pz_gen");
+  fTree->Branch("deltaR_lak8jet",&deltaR_lak8jet,"deltaR_lak8jet/F");
+  fTree->Branch("deltaphi_METak8jet",&deltaphi_METak8jet,"deltaphi_METak8jet/F");
+  fTree->Branch("deltaphi_Vak8jet",&deltaphi_Vak8jet,"deltaphi_Vak8jet/F");
+  fTree->Branch("v_pt",&v_pt,"v_pt/F");
+  fTree->Branch("v_eta",&v_eta,"v_eta/F");
+  fTree->Branch("v_phi",&v_phi,"v_phi/F");
+  fTree->Branch("v_mt",&v_mt,"v_mt/F");
+  fTree->Branch("mass_lvj_type0",&mass_lvj_type0,"mass_lvj_type0/F");
+  fTree->Branch("mass_lvj_type2",&mass_lvj_type2,"mass_lvj_type2/F");
+  fTree->Branch("vbf_maxpt_j1_pt",&vbf_maxpt_j1_pt,"vbf_maxpt_j1_pt/F");
+  fTree->Branch("vbf_maxpt_j1_eta",&vbf_maxpt_j1_eta,"vbf_maxpt_j1_eta/F");
+  fTree->Branch("vbf_maxpt_j1_phi",&vbf_maxpt_j1_phi,"vbf_maxpt_j1_phi/F");
+  fTree->Branch("vbf_maxpt_j1_e",&vbf_maxpt_j1_e,"vbf_maxpt_j1_e/F");
+  fTree->Branch("vbf_maxpt_j1_bDiscriminatorCSV",&vbf_maxpt_j1_bDiscriminatorCSV,"vbf_maxpt_j1_bDiscriminatorCSV/F");
+  fTree->Branch("vbf_maxpt_j2_pt",&vbf_maxpt_j2_pt,"vbf_maxpt_j2_pt/F");
+  fTree->Branch("vbf_maxpt_j2_eta",&vbf_maxpt_j2_eta,"vbf_maxpt_j2_eta/F");
+  fTree->Branch("vbf_maxpt_j2_phi",&vbf_maxpt_j2_phi,"vbf_maxpt_j2_phi/F");
+  fTree->Branch("vbf_maxpt_j2_e",&vbf_maxpt_j2_e,"vbf_maxpt_j2_e/F");
+  fTree->Branch("vbf_maxpt_j2_bDiscriminatorCSV",&vbf_maxpt_j2_bDiscriminatorCSV,"vbf_maxpt_j2_bDiscriminatorCSV/F");
+  fTree->Branch("vbf_maxpt_jj_pt",&vbf_maxpt_jj_pt,"vbf_maxpt_jj_pt/F");
+  fTree->Branch("vbf_maxpt_jj_eta",&vbf_maxpt_jj_eta,"vbf_maxpt_jj_eta/F");
+  fTree->Branch("vbf_maxpt_jj_phi",&vbf_maxpt_jj_phi,"vbf_maxpt_jj_phi/F");
+  fTree->Branch("vbf_maxpt_jj_m",&vbf_maxpt_jj_m,"vbf_maxpt_jj_m/F");
 }
 
