9cd37ba774066f35ca0ba401a4dadf83894d2cff
Luca Brianza <brianza@hercules.mib.infn.it>
2015-05-04 17:15:06 +0200

    changes for the synch

--- interface/setOutputTreeSynch.h
+++ interface/setOutputTreeSynch.h
@@ -54,6 +54,54 @@ class setOutputTreeSynch {
   float jet3_phi; //2nd most energetic AK4
   float jet3_btag; //2nd most energetic AK4 
   
+  int issignal;
+  float wSampleWeight;
+  float totalEventWeight;
+  float eff_and_pu_Weight;
+  float nu_pz_type0;
+  float nu_pz_type2;
+  float nu_pz_run2;
+  float nu_pz_run2_oth;
+  int nu_pz_run2_type;
+  int nu_pz_isre;
+  float l_e;
+  float jet_e;
+  float jet_mass_tr;
+  float jet_mass_fi;
+  float W_pt_gen;
+  float W_pz_gen;
+  float W_rap_gen;
+  float genGravMass;
+  float nu_pz_gen;
+  float nu_pt_gen;
+  float nu_phi_gen;
+  float nu_eta_gen;
+  float deltaR_lak8jet;
+  float deltaphi_METak8jet;
+  float deltaphi_Vak8jet;
+  float W_mt;
+  float mass_lvj_type0;
+  float mass_lvj_run2;
+  float mass_leptonic_closerjet;
+  float mass_ungroomedjet_closerjet;
+  int nBTagJet_loose;
+  int nBTagJet_tight;
+  float vbf_maxpt_j1_pt;
+  float vbf_maxpt_j1_eta;
+  float vbf_maxpt_j1_phi;
+  float vbf_maxpt_j1_e;
+  float vbf_maxpt_j1_bDiscriminatorCSV;
+  float vbf_maxpt_j2_pt;
+  float vbf_maxpt_j2_eta;
+  float vbf_maxpt_j2_phi;
+  float vbf_maxpt_j2_e;
+  float vbf_maxpt_j2_bDiscriminatorCSV;
+  float vbf_maxpt_jj_pt;
+  float vbf_maxpt_jj_eta;
+  float vbf_maxpt_jj_phi;
+  float vbf_maxpt_jj_m;
+
+
 
   setOutputTreeSynch(TTree* outputTree);
   //  setOutputTreeSynch(TTree *outputTree=0);
