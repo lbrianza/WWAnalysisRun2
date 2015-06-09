a708fe98b9fd6e4bfc55524282fb4c82eb4e1d64
Luca Brianza <brianza@hercules.mib.infn.it>
2015-06-09 09:29:32 +0200

    added gen info

---
+++ python/submit_on_hercules_MC74x.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+import os
+import glob
+import math
+from array import array
+import sys
+import time
+import subprocess
+
+currentDir = os.getcwd();
+CMSSWDir = currentDir+"../";
+ReducedTreeDir = "";
+
+name = ["RSGraviton600", "RSGraviton800", "RSGraviton1000", "RSGraviton1200", "RSGraviton1400", "RSGraviton1600", "RSGraviton1800", "RSGraviton2000",
+        "RSGraviton2500", "RSGraviton3000", "RSGraviton3500", "RSGraviton4000", "RSGraviton4500", 
+        "WJets100", "WJets200", "WJets400", "WJets600", "TTbar", "sch", "tch", "tWch", "tWch_bar", "WW", "ZZ"];
+category = ["mu","el"];
+xSecWeight = ["4.76735", "1.16691", "0.377865", "0.144482", "0.0616708", "0.0288651", "0.0141334", "0.00751431",
+              "0.00167726", "0.000443483", "0.000133915", "0.0000424117", "0.0000130705",
+              "1292.", "385.9", "47.9", "19.9", "831.76", "10.11", "216.99", "38.09", "38.09", "63.21", "10.32"];
+N = ["32354.", "31906.", "32448.", "32252.", "32275.", "31971.", "32021.", "31295.",
+     "32032.", "31374.", "32194.", "32207.", "31551.",
+     "10142187.", "5231856.", "1901705.", "1036108.", "42730273.", "984400.", "2966200.", "995600.", "1000000.", "994416.", "996168."];
+
+for a in range(len(category)):
+    for i in range(len(name)):
+        fn = "Job/Job_"+name[i]+"_"+category[a];
+        outScript = open(fn+".sh","w");
+        command = "python python/produceWWNtuples.py -i /gwteray/users/brianza/WWNtupleRun2/ReducedTree_MC74x/ -n ReducedSelection_"+name[i]+".root -o WWTree_"+name[i]+".root -l "+category[a]+" -w "+xSecWeight[i]+" -no "+N[i];
+        print command;
+        outScript.write('#!/bin/bash');
+        outScript.write("\n"+'cd '+CMSSWDir);
+        outScript.write("\n"+'eval `scram runtime -sh`');
+        outScript.write("\n"+'cd '+currentDir);
+        outScript.write("\n"+"unbuffer "+command+" > "+fn+"_output.txt");
+        outScript.close();
+        os.system("chmod 777 "+currentDir+"/"+fn+".sh");
+        os.system("qsub -V -d "+currentDir+" -q shortcms "+currentDir+"/"+fn+".sh");
