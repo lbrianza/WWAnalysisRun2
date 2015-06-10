5d2ce3f59a64d8604bf26500fe262d773ea5214a
Luca Brianza <brianza@hercules.mib.infn.it>
2015-06-10 17:00:11 +0200

    added info on AK8 closer jet and gen info

--- python/submit_on_hercules_MC74x.py
+++ python/submit_on_hercules_MC74x.py
@@ -21,12 +21,15 @@ xSecWeight = ["4.76735", "1.16691", "0.377865", "0.144482", "0.0616708", "0.0288
 N = ["32354.", "31906.", "32448.", "32252.", "32275.", "31971.", "32021.", "31295.",
      "32032.", "31374.", "32194.", "32207.", "31551.",
      "10142187.", "5231856.", "1901705.", "1036108.", "42730273.", "984400.", "2966200.", "995600.", "1000000.", "994416.", "996168."];
+mass = ["600", "800", "1000", "1200", "1400", "1600", "1800", "2000",
+        "2500", "3000", "3500", "4000", "4500", 
+        "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
 
 for a in range(len(category)):
     for i in range(len(name)):
         fn = "Job/Job_"+name[i]+"_"+category[a];
         outScript = open(fn+".sh","w");
-        command = "python python/produceWWNtuples.py -i /gwteray/users/brianza/WWNtupleRun2/ReducedTree_MC74x/ -n ReducedSelection_"+name[i]+".root -o WWTree_"+name[i]+".root -l "+category[a]+" -w "+xSecWeight[i]+" -no "+N[i];
+        command = "python python/produceWWNtuples.py -i /gwteray/users/brianza/WWNtupleRun2/ReducedTree_MC74x/ -n ReducedSelection_"+name[i]+".root -o WWTree_"+name[i]+".root -l "+category[a]+" -w "+xSecWeight[i]+" -no "+N[i]+" -mass "+mass[i];
         print command;
         outScript.write('#!/bin/bash');
         outScript.write("\n"+'cd '+CMSSWDir);
