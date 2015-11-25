4f8fd7204a71b641626b84ca442001ddc3e457f4
lbrianza <lucabrianza@gmail.com>
2015-11-25 20:00:56 +0100

    tool for interpolation

---
+++ interpolate.cpp
@@ -0,0 +1,242 @@
+//c++ -o interpolate interpolate.cpp `root-config --cflags --glibs`
+//to run:
+//./interpolate BulkG el_HPW WW
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TKey.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TProfile.h"
+#include "TF1.h"
+#include "TGraphAsymmErrors.h"
+#include "TString.h"
+#include "TCut.h"
+#include "TMath.h"
+#include "TApplication.h"
+#include "TError.h"
+#include "TCanvas.h"
+#include "TGraphErrors.h"
+#include "TPad.h"
+#include "TStyle.h"
+#include "TLorentzVector.h"
+#include "TChain.h"
+#include <sstream>
+#include "TMath.h"
+#include <cmath>
+
+using namespace std;
+
+double DoubleCB(Double_t *x,Double_t *par)
+{
+
+  //[0] = N
+  //[1] = mean
+  //[2] = sigma
+  //[3] = alpha
+  //[4] = n
+  //[5] = alpha2
+  //[6] = n2
+
+  double xx = x[0];
+  double mean   = par[2] ; // mean
+  double sigma  = par[6] ; // sigma of the positive side of the gaussian
+  // double sigmaN = par[3] ; // sigma of the negative side of the gaussian
+  double alpha  = par[0] ; // junction point on the positive side of the gaussian
+  double n      = par[3] ; // power of the power law on the positive side of the gaussian
+  double alpha2 = par[1] ; // junction point on the negative side of the gaussian
+  double n2     = par[4] ; // power of the power law on the negative side of the gaussian
+  double N      = par[5] ;
+
+  if ((xx-mean)/sigma > fabs(alpha))
+    {
+      double A = pow(n/fabs(alpha), n) * exp(-0.5 * alpha*alpha);
+      double B = n/fabs(alpha) - fabs(alpha);
+    
+      return N * A * pow(B + (xx-mean)/sigma, -1.*n);
+    }
+  
+  else if ((xx-mean)/sigma < -1.*fabs(alpha2))
+    {
+      double A = pow(n2/fabs(alpha2), n2) * exp(-0.5 * alpha2*alpha2);
+      double B = n2/fabs(alpha2) - fabs(alpha2);
+    
+      return N * A * pow(B - (xx-mean)/sigma, -1.*n2);
+    }
+  
+  else if ((xx-mean) > 0)
+    {
+      return N * exp(-1. * (xx-mean)*(xx-mean) / (2*sigma*sigma) );
+    }
+  
+  else
+    {
+      return N * exp(-1. * (xx-mean)*(xx-mean) / (2*sigma*sigma) );
+    }
+   
+}
+
+
+int main(int argc, char** argv)
+{
+  std::string signal =argv[1];
+  std::string category =argv[2];
+  std::string type =argv[3];
+
+  string line;
+  string line2;
+  ifstream logFile[11];
+  ifstream datacard[11];
+  int mass[11] = {800,1200,1400,1600,1800,2000,2500,3000,3500,4000,4500};
+  bool goOut=false;
+  int nPar = 8, nMass=11;
+  string namePar[8];
+  float val[8][11];
+  float err[8][11];
+  string parName[8]={"alpha1","alpha2","mean","n1","n2","number","sigma","Ndatacard"}; 
+
+  //  char signal[200]="BulkG_WW";
+  //  char category[200]="el_HPW";
+  //  char signal[200]={signalString.c_str()};
+  //char category[200]={categoryString.c_str()};
+
+  for(int iMass=0; iMass<nMass; iMass++)
+    {
+      TString NameFile = Form("log/%s_%s_M%d_%s.log",signal.c_str(),type.c_str(),mass[iMass],category.c_str());
+      logFile[iMass].open(NameFile.Data());
+      if (logFile[iMass]) 
+	{
+	  goOut = false;
+	  while (getline(logFile[iMass],line) && !goOut) 
+	    {
+	      if (TString(line).Contains("  --------------------  --------------------------")) 
+		{
+		  std::cout<<"file: "<<NameFile.Data()<<" content: "<<line<<std::endl;
+		  for (int par=0; par<nPar-1; par++) 
+		    {
+		      string pm;
+		      //		      getline(logFile[i],line);
+		      //std::cout<<"file: "<<NameFile.Data()<<" content: "<<line<<std::endl;
+		      //		      std::stringstream linestream(line);
+		      //		      std::cout<<linestream<<std::endl;
+		      logFile[iMass] >> namePar[par] >> val[par][iMass] >> pm >> err[par][iMass];		      
+		      std::cout<<"par: "<<namePar[par]<<" val: "<<val[par][iMass]<<" err: "<<err[par][iMass]<<std::endl;
+		    }		  
+		  goOut = true;
+		}	    
+	    }
+	}
+      logFile[iMass].close();
+
+      TString NameCard = Form("/afs/cern.ch/user/l/lbrianza/work/PHD/WW_ANALYSIS_RUN2/WWSEMILEP/ANALISI_NOVEMBER/TEST/CMSSW_7_1_5/src/13TeV_datacards_Spring15/cards_%s/cards_%s/wwlvj_%s_%s_lvjj_M%d_%s_unbin.txt",signal.c_str(),category.c_str(),signal.c_str(),type.c_str(),mass[iMass],category.c_str());
+      std::cout<<"open datacard: "<<NameCard.Data()<<std::endl;
+
+      datacard[iMass].open(NameCard.Data());
+      if (datacard[iMass]) 
+	{
+	  goOut = false;
+	  while (getline(datacard[iMass],line2) && !goOut) 
+	    {
+	      //	      std::cout<<line2<<std::endl;
+	      if (TString(line2).Contains("process")) 
+		{
+		  string rate;
+		  getline(datacard[iMass],line2);
+		  std::cout<<"file: "<<NameFile.Data()<<" content: "<<line2<<std::endl;
+		  datacard[iMass] >> rate >> val[nPar-1][iMass];
+		  err[nPar-1][iMass]=0;		      
+		  std::cout<<"par: "<<namePar[nPar-1]<<" val: "<<val[nPar-1][iMass]<<" err: "<<err[nPar-1][iMass]<<std::endl;
+		  goOut = true;
+		}	    
+	    }
+	}
+      
+    }
+
+  TString NameOutput = Form("%s_%s_%s.root",signal.c_str(),category.c_str(),type.c_str());
+  TFile *outputFile = new TFile(NameOutput.Data(),"RECREATE");
+  outputFile->cd();
+
+  TGraphErrors *gPar[8];
+  TF1 *fitPar[8];
+
+  ofstream param;
+  param.open((string("param_")+NameOutput.Data()+string(".txt")).c_str());
+
+  for (int iPar=0; iPar<nPar; iPar++)
+    {
+      TString NameFile = Form("%s",parName[iPar].c_str());
+      TString NameFunc = Form("fit_%s",parName[iPar].c_str());
+      //gPar[iPar] = new TGraphErrors(NameFile,NameFile);
+      gPar[iPar] = new TGraphErrors();
+      //      fitPar[iPar] = new TF1("pol1","pol1",2,1000,3600);
+
+      for (int iMass=0; iMass<nMass; iMass++)
+	{
+	  gPar[iPar]->SetPoint(iMass,mass[iMass],val[iPar][iMass]);
+	  gPar[iPar]->SetPointError(iMass,0,err[iPar][iMass]);
+	}
+      /*
+      param << parName[iPar]<<" : ";
+      if (TString(parName[iPar]).Contains("number") || TString(parName[iPar]).Contains("n1") || TString(parName[iPar]).Contains("n2")) {
+	gPar[iPar]->Fit("pol3");
+	TF1 *func = gPar[iPar]->GetFunction("pol3");
+	//	func->SetRange(0,3600);
+	gPar[iPar]->Fit(func);
+	if (func->GetParameter(1)>0 && func->GetParameter(2)>0 && func->GetParameter(3)>0 )
+	  param<<func->GetParameter(0)<<"+"<<func->GetParameter(1)<<"*x+"<<func->GetParameter(2)<<"*x*x+"<<func->GetParameter(3)<<"*x*x*x"<<std::endl;
+	else if (func->GetParameter(1)>0 && func->GetParameter(2)>0 && func->GetParameter(3)<0 )
+	  param<<func->GetParameter(0)<<"+"<<func->GetParameter(1)<<"*x+"<<func->GetParameter(2)<<"*x*x"<<func->GetParameter(3)<<"*x*x*x"<<std::endl;
+	else if (func->GetParameter(1)>0 && func->GetParameter(2)<0 && func->GetParameter(3)>0 )
+	  param<<func->GetParameter(0)<<"+"<<func->GetParameter(1)<<"*x"<<func->GetParameter(2)<<"*x*x+"<<func->GetParameter(3)<<"*x*x*x"<<std::endl;
+	else if (func->GetParameter(1)>0 && func->GetParameter(2)<0 && func->GetParameter(3)<0 )
+	  param<<func->GetParameter(0)<<"+"<<func->GetParameter(1)<<"*x"<<func->GetParameter(2)<<"*x*x"<<func->GetParameter(3)<<"*x*x*x"<<std::endl;
+	else if (func->GetParameter(1)<0 && func->GetParameter(2)>0 && func->GetParameter(3)>0 )
+	  param<<func->GetParameter(0)<<func->GetParameter(1)<<"*x+"<<func->GetParameter(2)<<"*x*x+"<<func->GetParameter(3)<<"*x*x*x"<<std::endl;
+	else if (func->GetParameter(1)<0 && func->GetParameter(2)>0 && func->GetParameter(3)<0 )
+	  param<<func->GetParameter(0)<<func->GetParameter(1)<<"*x+"<<func->GetParameter(2)<<"*x*x"<<func->GetParameter(3)<<"*x*x*x"<<std::endl;
+	else if (func->GetParameter(1)<0 && func->GetParameter(2)<0 && func->GetParameter(3)>0 )
+	  param<<func->GetParameter(0)<<func->GetParameter(1)<<"*x"<<func->GetParameter(2)<<"*x*x+"<<func->GetParameter(3)<<"*x*x*x"<<std::endl;
+	else if (func->GetParameter(1)<0 && func->GetParameter(2)<0 && func->GetParameter(3)<0 )
+	  param<<func->GetParameter(0)<<func->GetParameter(1)<<"*x"<<func->GetParameter(2)<<"*x*x"<<func->GetParameter(3)<<"*x*x*x"<<std::endl;
+      }
+      else if (TString(parName[iPar]).Contains("n1") || TString(parName[iPar]).Contains("n2")) {
+	gPar[iPar]->Fit("pol2");
+	TF1 *func = gPar[iPar]->GetFunction("pol2");
+	//	func->SetRange(0,3600);
+	gPar[iPar]->Fit(func);
+	if (func->GetParameter(1)>0 && func->GetParameter(2)>0 )
+	  param<<func->GetParameter(0)<<"+"<<func->GetParameter(1)<<"*x+"<<func->GetParameter(2)<<"*x*x"<<std::endl;
+	else if (func->GetParameter(1)>0 && func->GetParameter(2)<0 )
+	  param<<func->GetParameter(0)<<"+"<<func->GetParameter(1)<<"*x"<<func->GetParameter(2)<<"*x*x"<<std::endl;
+	else if (func->GetParameter(1)<0 && func->GetParameter(2)>0 )
+	  param<<func->GetParameter(0)<<func->GetParameter(1)<<"*x+"<<func->GetParameter(2)<<"*x*x"<<std::endl;
+	else if (func->GetParameter(1)<0 && func->GetParameter(2)<0 )
+	  param<<func->GetParameter(0)<<func->GetParameter(1)<<"*x"<<func->GetParameter(2)<<"*x*x"<<std::endl;
+      }
+      else {
+	gPar[iPar]->Fit("pol1");
+	TF1 *func = gPar[iPar]->GetFunction("pol1");
+	//func->SetRange(0,3600);
+	gPar[iPar]->Fit(func);
+	if (func->GetParameter(1)>0)
+	  param<<func->GetParameter(0)<<"+"<<func->GetParameter(1)<<"*x"<<std::endl;
+	else
+	  param<<func->GetParameter(0)<<func->GetParameter(1)<<"*x"<<std::endl;
+      }
+      */
+      gPar[iPar]->GetXaxis()->SetTitle("mass (GeV)");
+      gPar[iPar]->GetYaxis()->SetTitle(parName[iPar].c_str());
+      gPar[iPar]->Write(NameFile.Data());
+    }
+
+  outputFile->Close();
+  
+  
+  return 0;
+}
---
+++ read_and_plot.cpp
@@ -0,0 +1,155 @@
+//c++ -o read_and_plot read_and_plot.cpp `root-config --cflags --glibs`
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TKey.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TProfile.h"
+#include "TF1.h"
+#include "TGraphAsymmErrors.h"
+#include "TString.h"
+#include "TCut.h"
+#include "TMath.h"
+#include "TApplication.h"
+#include "TError.h"
+#include "TCanvas.h"
+#include "TGraphErrors.h"
+#include "TPad.h"
+#include "TStyle.h"
+#include "TLorentzVector.h"
+#include "TChain.h"
+#include <sstream>
+#include "TMath.h"
+#include <cmath>
+
+using namespace std;
+
+double DoubleCB(Double_t *x,Double_t *par)
+{
+
+  //[0] = N
+  //[1] = mean
+  //[2] = sigma
+  //[3] = alpha
+  //[4] = n
+  //[5] = alpha2
+  //[6] = n2
+
+  double xx = x[0];
+  double mean   = par[2] ; // mean
+  double sigma  = par[6] ; // sigma of the positive side of the gaussian
+  // double sigmaN = par[3] ; // sigma of the negative side of the gaussian
+  double alpha  = par[0] ; // junction point on the positive side of the gaussian
+  double n      = par[3] ; // power of the power law on the positive side of the gaussian
+  double alpha2 = par[1] ; // junction point on the negative side of the gaussian
+  double n2     = par[4] ; // power of the power law on the negative side of the gaussian
+  double N      = par[7] ;
+
+  if ((xx-mean)/sigma > fabs(alpha))
+    {
+      double A = pow(n/fabs(alpha), n) * exp(-0.5 * alpha*alpha);
+      double B = n/fabs(alpha) - fabs(alpha);
+    
+      return N * A * pow(B + (xx-mean)/sigma, -1.*n);
+    }
+  
+  else if ((xx-mean)/sigma < -1.*fabs(alpha2))
+    {
+      double A = pow(n2/fabs(alpha2), n2) * exp(-0.5 * alpha2*alpha2);
+      double B = n2/fabs(alpha2) - fabs(alpha2);
+    
+      return N * A * pow(B - (xx-mean)/sigma, -1.*n2);
+    }
+  
+  else if ((xx-mean) > 0)
+    {
+      return N * exp(-1. * (xx-mean)*(xx-mean) / (2*sigma*sigma) );
+    }
+  
+  else
+    {
+      return N * exp(-1. * (xx-mean)*(xx-mean) / (2*sigma*sigma) );
+    }
+   
+}
+
+
+int main(int argc, char** argv)
+{
+  std::string signal =argv[1];
+  std::string category =argv[2];
+  std::string type =argv[3];
+
+  string line;
+  ifstream logFile[11];
+  bool goOut=false;
+  int nPar = 8;
+  string namePar[8];
+  Double_t val[8][11];
+  Double_t err[8][11];
+  string parName[8]={"alpha1","alpha2","mean","n1","n2","number","sigma","Ndatacard"}; 
+
+  TString *readFile = new TString (Form("%s_%s_%s.root",signal.c_str(),category.c_str(),type.c_str()));
+  TFile* inputFile = new TFile(readFile->Data());
+
+  std::cout<<"open: "<<readFile->Data()<<std::endl;
+  //  inputFile->cd();
+
+  TGraph *gPar[8];
+  Double_t paramVec[8];
+  TF1* fTest[100];
+
+  for (int iPar=0; iPar<nPar; iPar++)
+    {
+      TString NameFile = Form("%s",parName[iPar].c_str());
+      gPar[iPar] = (TGraph*)inputFile->Get(NameFile.Data());
+    }
+
+  int i=0;
+  for (int mass=800; mass<5001; mass+=100,i++) {
+
+    TString NameFunc = Form("f_%d",mass);
+    fTest[i] = new TF1 (NameFunc.Data(), DoubleCB, 0, 10000, 8) ;
+
+    for (int iPar=0; iPar<nPar; iPar++) {
+      paramVec[iPar] = gPar[iPar]->Eval(mass);
+      std::cout<<"mass: "<<mass<<" par: "<<paramVec[iPar]<<std::endl;
+    }
+    //    if (mass==4200) std::cout<<paramVec[8]<<std::endl;
+    fTest[i]->SetParameters (paramVec) ;
+    fTest[i]->SetLineColor (kBlue-9+(i%5));
+    //fTest[i]->SetLineColor (kGreen);
+  }
+
+  inputFile->Close();
+
+  TCanvas *c = new TCanvas (Form("%s_%s_%s",signal.c_str(),category.c_str(),type.c_str()) ,"",500,525);
+  c->cd();
+  c->DrawFrame(0,0,5000,1000);
+
+  i=0;
+  for (int mass=800; mass<5001; mass+=100,i++) {
+    //    if (mass!=4200) continue;
+    //else fTest[i]->Draw();
+    if (i==0)
+      fTest[i]->Draw();
+    else
+      fTest[i]->Draw("same");
+    
+  }
+
+  TString NameOutput = Form("extrapolation_%s_%s_%s.root",signal.c_str(),category.c_str(),type.c_str());
+  TFile *outputFile = new TFile(NameOutput.Data(),"RECREATE");
+  outputFile->cd();
+
+  c->Write();
+    
+  
+  return 0;
+}
