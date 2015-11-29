4d3189f124c840b8d89cab6a01d48e1da4585952
lbrianza <lucabrianza@gmail.com>
2015-11-29 16:19:26 +0100

    change from bool to int isMC option

--- python/produceWWNtuples.py
+++ python/produceWWNtuples.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser (description = 'produce ntuples with WW semileptonic final state')
     parser.add_argument ('-i', '--inputFolder' , default = '/eos/cms/store/caf/user/lbrianza/WWReducedTree_run2' , help='input folder with the reduced trees')
     parser.add_argument ('-o', '--output' , default = 'RSGraviton4000', help='output file')
-    parser.add_argument ('-mc', '--ismc' , default = 'False', help='is MC or not')
+    parser.add_argument ('-mc', '--ismc' , default = '0', help='is MC or not')
     parser.add_argument ('-l', '--lepton' , default = 'mu', help='lepton category (mu or el)')
     parser.add_argument ('-t', '--tree' , default = 'TreeMaker2/PreSelection', help='name of the input tree')
     parser.add_argument ('-n', '--name' , default = 'RSGraviton4000' , help='input file')
