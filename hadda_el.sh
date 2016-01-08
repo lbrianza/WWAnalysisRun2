68a61f47547202d0ab6763872219c88829a790c0
lbrianza <lucabrianza@gmail.com>
2016-01-08 11:42:54 +0100

    bug fix

--- hadda_el.sh
+++ hadda_el.sh
@@ -5,5 +5,5 @@ hadd -f WWTree_WZ_excl.root WWTree_WZ_excl_*
 hadd -f WWTree_ZZ_excl.root WWTree_ZZ_excl_*
 mv WWTree_WJets.root WWTree_WJets_incl.root
 hadd -f WWTree_WJets.root WWTree_WJets100.root WWTree_WJets200.root WWTree_WJets400.root WWTree_WJets600bis.root WWTree_WJets800.root WWTree_WJets1200.root WWTree_WJets2500.root 
-hadd -f WWTree_VV.root WWTree_WW.root WWTree_WZ_excl.root WWTree_ZZ_excl.root 
+hadd -f WWTree_VV.root WWTree_WW_excl.root WWTree_WZ_excl.root WWTree_ZZ_excl.root 
 hadd -f WWTree_pseudodata.root WWTree_WJets.root WWTree_TTbar.root WWTree_VV.root WWTree_STop.root
