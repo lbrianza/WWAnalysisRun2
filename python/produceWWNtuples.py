712666e36c05038e15aef8e3a8473f461e61a1f5
lbrianza <lucabrianza@gmail.com>
2016-04-29 17:23:41 +0200

    add option to run in local

--- python/produceWWNtuples.py
+++ python/produceWWNtuples.py
@@ -19,7 +19,8 @@ if __name__ == '__main__':
     parser.add_argument ('-mass', '--mass' , default = '1000' , help='mass of input signal')
     parser.add_argument ('-trig', '--applyTrigger' , default = '0' , help='apply trigger or not')
     parser.add_argument ('-json', '--json', default = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt', help="json file to apply")
+    parser.add_argument ('-loc', '--isLocal' , default = '0', help='run in local or not')
     args = parser.parse_args ()
 
-    print 'produceWWNtuples '+args.inputFolder+' '+args.output+' '+args.ismc+' '+args.lepton+' '+args.tree+' '+args.name+' '+args.xsecWeight+' '+args.numberOfEntries+' '+args.mass+' '+args.applyTrigger+' '+args.json
-    os.system('./produceWWNtuples.exe '+args.inputFolder+' '+args.output+' '+args.ismc+' '+args.lepton+' '+args.tree+' '+args.name+' '+args.xsecWeight+' '+args.numberOfEntries+' '+args.mass+' '+args.applyTrigger+' '+args.json)
+    print 'produceWWNtuples '+args.inputFolder+' '+args.output+' '+args.ismc+' '+args.lepton+' '+args.tree+' '+args.name+' '+args.xsecWeight+' '+args.numberOfEntries+' '+args.mass+' '+args.applyTrigger+' '+args.json+' '+args.isLocal
+    os.system('./produceWWNtuples.exe '+args.inputFolder+' '+args.output+' '+args.ismc+' '+args.lepton+' '+args.tree+' '+args.name+' '+args.xsecWeight+' '+args.numberOfEntries+' '+args.mass+' '+args.applyTrigger+' '+args.json+' '+args.isLocal)
