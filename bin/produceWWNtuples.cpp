a374ef9ec68f8906476e6a1fd071690fa5eb06e4
Andrea <andrea.benaglia@cern.ch>
2016-06-13 19:47:15 +0200

    fix PuppiAK4 index

--- bin/produceWWNtuples.cpp
+++ bin/produceWWNtuples.cpp
@@ -37,80 +37,36 @@ using namespace std;
 //*****PU WEIGHT***************
 
 vector<double> generate_weights(TH1* data_npu_estimated, int isForSynch){
-  // see SimGeneral/MixingModule/python/mix_2015_25ns_Startup_PoissonOOTPU_cfi.pyy; copy and paste from there:
-  const double npu_probs[52] = {
-                        4.8551E-07,
-                        1.74806E-06,
-                        3.30868E-06,
-                        1.62972E-05,
-                        4.95667E-05,
-                        0.000606966,
-                        0.003307249,
-                        0.010340741,
-                        0.022852296,
-                        0.041948781,
-                        0.058609363,
-                        0.067475755,
-                        0.072817826,
-                        0.075931405,
-                        0.076782504,
-                        0.076202319,
-                        0.074502547,
-                        0.072355135,
-                        0.069642102,
-                        0.064920999,
-                        0.05725576,
-                        0.047289348,
-                        0.036528446,
-                        0.026376131,
-                        0.017806872,
-                        0.011249422,
-                        0.006643385,
-                        0.003662904,
-                        0.001899681,
-                        0.00095614,
-                        0.00050028,
-                        0.000297353,
-                        0.000208717,
-                        0.000165856,
-                        0.000139974,
-                        0.000120481,
-                        0.000103826,
-                        8.88868E-05,
-                        7.53323E-05,
-                        6.30863E-05,
-                        5.21356E-05,
-                        4.24754E-05,
-                        3.40876E-05,
-                        2.69282E-05,
-                        2.09267E-05,
-                        1.5989E-05,
-                        4.8551E-06,
-                        2.42755E-06,
-                        4.8551E-07,
-                        2.42755E-07,
-                        1.21378E-07,
-                        4.8551E-08
-};
+  // see SimGeneral/MixingModule/python/mix_2015_25ns_FallMC_matchData_PoissonOOTPU_cfi.py; copy and paste from there:
+  const double npu_probs[50] = { 0.000108643, 0.000388957, 0.000332882, 0.00038397,  0.000549167,
+                                 0.00105412,  0.00459007,  0.0210314,   0.0573688,   0.103986,
+                                 0.142369,    0.157729,    0.147685,    0.121027,    0.08855,
+                                 0.0582866,   0.0348526,   0.019457,    0.0107907,   0.00654313,
+                                 0.00463195,  0.00370927,  0.0031137,   0.00261141,  0.00215499,
+                                 0.00174491,  0.00138268,  0.00106731,  0.000798828, 0.00057785,
+                                 0.00040336,  0.00027161,  0.000176535, 0.00011092,  6.75502e-05,
+                                 4.00323e-05, 2.32123e-05, 1.32585e-05, 7.51611e-06, 4.25902e-06,
+                                 2.42513e-06, 1.39077e-06, 8.02452e-07, 4.64159e-07, 2.67845e-07,
+                                 1.5344e-07,  8.68966e-08, 4.84931e-08, 2.6606e-08,  1.433e-08};
   
   if (isForSynch==0) { //OFFICIAL RECIPE
-    vector<double> result(52);
+    vector<double> result(50);
     double s = 0.0;
-    for(int npu=0; npu<52; ++npu){
+    for(int npu=0; npu<50; ++npu){
       double npu_estimated = data_npu_estimated->GetBinContent(data_npu_estimated->GetXaxis()->FindBin(npu));                              
       result[npu] = npu_estimated / npu_probs[npu];
       s += npu_estimated;
     }
     // normalize weights such that the total sum of weights over thw whole sample is 1.0, i.e., sum_i  result[i] * npu_probs[i] should be 1.0 (!)
-    for(int npu=0; npu<52; ++npu){
+    for(int npu=0; npu<50; ++npu){
       result[npu] /= s;
     }
     return result;
   }
 
   else { //THIS IS FOR THE SYNCH ONLY. THIS IS NOT THE OFFICIAL RECIPE!
-    vector<double> result(60);
-    for(int npu=0; npu<60; ++npu){
+    vector<double> result(50);
+    for(int npu=0; npu<50; ++npu){
       if (data_npu_estimated->GetBinContent(data_npu_estimated->GetXaxis()->FindBin(npu))==0.)
 	result[npu] = 0.;
       else {
@@ -1091,7 +1047,7 @@ int main (int argc, char** argv)
     tempPt1 = 0.; int pos1Puppi = -1;
     tempPt2 = 0.; int pos2Puppi = -1;
     int nGoodPuppiAK4jets=0;
-    for (unsigned int i=0; i<ReducedTree->JetsPuppiNum; i++) //loop on AK4 jet
+    for (unsigned int i=0; i<ReducedTree->JetsPuppiNum; i++) //loop on PuppiAK4 jet
     {
       bool isCleanedJet = true;
       if (ReducedTree->JetsPuppi_PtCorr[i]<=30 || ReducedTree->JetsPuppiPt[i]<=20 || fabs(ReducedTree->JetsPuppiEta[i])>=2.4)  continue;
@@ -1124,17 +1080,17 @@ int main (int argc, char** argv)
         WWTree->PuppiAK4_jet1_phi = ReducedTree->JetsPuppiPhi[i];
         WWTree->PuppiAK4_jet1_e   = ReducedTree->JetsPuppi_ECorr[i];
         tempPt1 = WWTree->PuppiAK4_jet1_pt;
-        if (pos1!=-1)
+        if (pos1Puppi!=-1)
         {
-          WWTree->PuppiAK4_jet2_pt  = ReducedTree->JetsPuppi_PtCorr[pos1];
-          WWTree->PuppiAK4_jet2_pt_jes_up = (ReducedTree->JetsPuppi_PtCorr[pos1]/ReducedTree->JetsPuppi_AK4correction[pos1])*ReducedTree->JetsPuppi_AK4correctionUp[pos1];
-          WWTree->PuppiAK4_jet2_pt_jes_dn = (ReducedTree->JetsPuppi_PtCorr[pos1]/ReducedTree->JetsPuppi_AK4correction[pos1])*ReducedTree->JetsPuppi_AK4correctionDown[pos1];
-          WWTree->PuppiAK4_jet2_eta = ReducedTree->JetsPuppiEta[pos1];
-          WWTree->PuppiAK4_jet2_phi = ReducedTree->JetsPuppiPhi[pos1];
-          WWTree->PuppiAK4_jet2_e   = ReducedTree->JetsPuppi_ECorr[pos1];
+          WWTree->PuppiAK4_jet2_pt  = ReducedTree->JetsPuppi_PtCorr[pos1Puppi];
+          WWTree->PuppiAK4_jet2_pt_jes_up = (ReducedTree->JetsPuppi_PtCorr[pos1Puppi]/ReducedTree->JetsPuppi_AK4correction[pos1Puppi])*ReducedTree->JetsPuppi_AK4correctionUp[pos1Puppi];
+          WWTree->PuppiAK4_jet2_pt_jes_dn = (ReducedTree->JetsPuppi_PtCorr[pos1Puppi]/ReducedTree->JetsPuppi_AK4correction[pos1Puppi])*ReducedTree->JetsPuppi_AK4correctionDown[pos1Puppi];
+          WWTree->PuppiAK4_jet2_eta = ReducedTree->JetsPuppiEta[pos1Puppi];
+          WWTree->PuppiAK4_jet2_phi = ReducedTree->JetsPuppiPhi[pos1Puppi];
+          WWTree->PuppiAK4_jet2_e   = ReducedTree->JetsPuppi_ECorr[pos1Puppi];
           tempPt2 = WWTree->PuppiAK4_jet2_pt;
         }
-        pos1 = i;
+        pos1Puppi = i;
         nGoodPuppiAK4jets++;
       }
       else if (ReducedTree->JetsPuppi_PtCorr[i]>tempPt2)
@@ -1146,7 +1102,7 @@ int main (int argc, char** argv)
         WWTree->PuppiAK4_jet2_phi = ReducedTree->JetsPuppiPhi[i];
         WWTree->PuppiAK4_jet2_e   = ReducedTree->JetsPuppi_ECorr[i];
         tempPt2 = WWTree->PuppiAK4_jet2_pt;
-        pos2 = i;
+        pos2Puppi = i;
         nGoodPuppiAK4jets++;
       }
     }
